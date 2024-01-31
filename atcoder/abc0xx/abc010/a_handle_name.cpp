bf0037a17f08059758c9a9bebf97346786058efb
kento <kento@toriwatari.bird>
2024-01-31 20:03:27 +0900

    🧒 Update

---
+++ atcoder/abc0xx/abc010/a_handle_name.cpp
@@ -0,0 +1,16 @@
+// https://atcoder.jp/contests/abc010/tasks/abc010_1
+
+#include <iostream>
+using namespace std;
+
+string func(string s) {
+    return s + "pp";
+}
+
+int main() {
+    cout << func("chokudai") << endl;
+    // chokudaipp
+
+    cout << func("sanagi") << endl;
+    // sanagipp
+}
