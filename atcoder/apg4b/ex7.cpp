5fac9ce0b2d387ab28ed4729515b88035b1326d0
kento <kento@toriwatari.bird>
2024-02-09 21:49:14 +0900

    🖤 Update

---
+++ atcoder/apg4b/ex7.cpp
@@ -0,0 +1,37 @@
+// https://atcoder.jp/contests/APG4b/tasks/APG4b_cp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool a = true;
+    bool b = false;
+    bool c = true;
+
+    if (a) {
+        cout << "At";
+    }
+    else {
+        cout << "Yo";
+    }
+
+    if (!a && b) {
+        cout << "Bo";
+    }
+    else if (!b || c) {
+        cout << "Co";
+    }
+
+    if (a && b && c) {
+        cout << "foo!";
+    }
+    else if (true && false) {
+        cout << "yeah!";
+    }
+    else if (!a || c) {
+        cout << "der";
+    }
+
+    cout << endl;
+    //=> AtCoder
+}
